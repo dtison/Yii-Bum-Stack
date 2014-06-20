965f3d921230418ffe2aaab84cf33d37584cc1c8
Matt McFarland <matt@gooddaytools.com>
2014-06-20 00:29:46 -0400

    remove ref to deprecated plugin

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,6 @@
 module.exports = function(grunt) {
 
 
-    require("grunt-load-gruntfile")(grunt);
-
-    var dir;
-
     process.chdir('./src/sponsor');
     require(process.cwd() + "/Gruntfile.js")(grunt);
 
